b130bcdbe395d20b49a945d5283392751d985642
TalitaFavore <132314574+TalitaFavore@users.noreply.github.com>
2023-08-28 11:43:18 -0300

    AreaCircunferencia.java

--- AreaCircunferencia.java
+++ AreaCircunferencia.java
@@ -9,7 +9,7 @@ public class AreaCircunferencia {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        final double pi = 3.14159;
+        final double pi = 3.14159; //definição de constante
         double area, raio;
         System.out.println("Informe o valor do raio da circunferência: ");
         raio = scanner.nextDouble();
