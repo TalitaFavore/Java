4756f71b649ddc9d4546ab2bd9b767167cdcf636
TalitaFavore <132314574+TalitaFavore@users.noreply.github.com>
2023-09-01 08:54:22 -0300

    AreaTriangulo2.java

--- AreaTriangulo2.java
+++ AreaTriangulo2.java
@@ -1,6 +1,7 @@
 package com.mycompany.areatriangulo2;
 /**
  * @author Talita
+ * (area = pi * Math.pow(raio,2)) é uma fórmula que também pode ser usada. É recomendado que se use o método 'pow' apanas quando a exponenciação for maior que 2.
  * "Double.parseDouble" converte de String para Double, já que tudo o que é informado no campo de "JOptionPane.showInputDialog" é interpretado 
  *como uma String, e é necessário que esteja em double para realizar o cálculo.
  */
